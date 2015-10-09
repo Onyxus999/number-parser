8cebd56ed7cefe8f2cef637569d1860afc8e002a
Rogers-Boyer Family <rogers-boyer@rogersboyer-Predator-G3610>
2015-10-08 17:13:05 -0700

    Created project outline. Started numParse.c to implement functions to parse numbers into their digits.

---
+++ lib/source/numParse.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+/***									***
+ ***	This module defines functions to handle parsing of numbers	***
+ ***		into strings and strings of digits. 			***
+ ***									***
+ ***	Author: Onyxus999 (Jacob Rogers)				***/
+
+/*
+	This function takes an int and returns a pointer to an array of its digits.
+*/
+int* digitize(int num)
+{
+
+}
+
+/*
+	This function takes an int and returns a c-string of its digits.	
+*/
+char* charitize(int num)
+{
+
+}
