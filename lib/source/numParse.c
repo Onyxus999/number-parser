4c6bf115943e62c63fba1dcd4210cfc8e48b6c0b
Onyxus Nine <self.onyxus@gmail.com>
2015-10-08 21:14:30 -0700

    Nearly finished numParse.c. Still need to fix all bugs and add comments.

--- lib/source/numParse.c
+++ lib/source/numParse.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 /***									***
@@ -15,16 +16,18 @@ int count_dig(int num)
 	int up_bound = 10;
 	int num_digits = 1;
 
-	// If the number is smaller than the upper bound, return the number of digits
-	if (num < up_bound) {
-		return num_digits;
-	}
-	// If the number is greater than the upper bound, raise the upper bound by a factor of 10 and add 1 to the number of digits
-	// (So e.g. 32 > 10 -> up_bound = 100, num_digits = 2 -> 32 < 100 -> return num_digits)
-	else {
-		up_bound *= 10;
-		num_digits++;
-		count_dig(num);
+	while (1==1)
+	{
+		// If the number is smaller than the upper bound, return the number of digits
+		if (num < up_bound) {
+			return num_digits;
+		}
+		// If the number is greater than the upper bound, raise the upper bound by a factor of 10 and add 1 to the number of digits
+		// (So e.g. 32 > 10 -> up_bound = 100, num_digits = 2 -> 32 < 100 -> return num_digits)
+		else {
+			up_bound *= 10;
+			num_digits++;
+		}
 	}
 
 }
@@ -34,7 +37,7 @@ int count_dig(int num)
  */
 int digital(int num, int i)
 {
-	return modf(num, pow(10, i+1)) / pow(10, i);
+	return ((num % (int) pow(10, i+1)) / pow(10, i));
 }
 
 /*
@@ -42,7 +45,9 @@ int digital(int num, int i)
  */
 char charital(int num, int i)
 {
-
+	char char_dig;
+	snprintf(&char_dig, sizeof(char_dig), "%d", digital(num, i));
+	return char_dig;
 }
 
 /*
@@ -51,6 +56,15 @@ char charital(int num, int i)
 char* charitize(int num)
 {
 
+	char* str_digits = malloc(sizeof(int) * count_dig(num));
+
+	for (int i = 0; i < count_dig(num); i++)
+	{
+		str_digits[i] = charital(num, i);				
+	}
+
+	return str_digits;
+
 }
 
 /*
@@ -58,5 +72,14 @@ char* charitize(int num)
  */
 int* digitize(int num)
 {
+
+	int* lst_digits = malloc(sizeof(int) * count_dig(num));
+
+	for (int i = 0; i < count_dig(num); i++)
+	{
+		lst_digits[i] = digital(num, i);
+	}
+
+	return lst_digits;
 
 }
