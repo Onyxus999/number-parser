e3b2ab0f4c0de63acdafd01f0fec6bbff33449f2
Onyxus Nine <self.onyxus@gmail.com>
2015-10-08 18:29:46 -0700

    Finished the outline of numParse.h

--- include/numParse.h
+++ include/numParse.h
@@ -6,11 +6,22 @@
  ***	Author: Onyxus999 (Jacob Rogers)	***/
 
 
-
+/*
+ *	This function takes an int and returns a pointer to an array of its digits
+ */
 extern int* digitize(int num);
 
+/*
+ *	This function takes an int and returns a c-string of its digits
+ */
 extern char* charitize(int num);
 
+/*
+ *	This function takes an int and an index and returns the number's ith digit
+ */
 extern int digital(int num, int i);
 
+/*
+ *	This function takes an int and an index and returns the number's ith digit as a char
+ */
 extern char charital(int num, int i);
