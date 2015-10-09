8d93e687014226014dc5d57be78a0bda0e2ca579
Onyxus Nine <self.onyxus@gmail.com>
2015-10-08 18:21:24 -0700

    Added basic outline to numParse.h

--- include/numParse.h
+++ include/numParse.h
@@ -1,1 +1,16 @@
+/***						***
+ ***	This library includes functions		***
+ ***	to parse numbers into their		***
+ ***	digits.					***
+ ***						***
+ ***	Author: Onyxus999 (Jacob Rogers)	***/
 
+
+
+extern int* digitize(int num);
+
+extern char* charitize(int num);
+
+extern int digital(int num, int i);
+
+extern char charital(int num, int i);
